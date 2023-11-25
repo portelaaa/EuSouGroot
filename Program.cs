2907b3410056d8ac072e7bed0a4701e1acf693fa
portelaaa <e107ti@etec.sp.gov.br>
2023-11-24 22:21:39 -0300

    Exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("...Groot Chatbot...");
+
+const string frasesaida = "tchau";
+
+string questao;
+do
+{
+    Console.Write("Pergunta: ");
+    questao = Console.ReadLine()!;
+
+    string resposta = ObtemRespostaGroot(questao);
+
+    ExibeResposta(ObtemRespostaGroot(questao));
+    
+} while (questao.Trim().ToLower() != frasesaida);
+
+void ExibeResposta (string questao)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"                           Resposta: {questao}");
+    Console.ResetColor();
+}
+
+string ObtemRespostaGroot(string questao)
+{
+    return questao.Trim().ToLower() == frasesaida ?
+       "Eu sou Groot" :
+       "Eu sou Groot.";
+       
+
+}
